ceb2f64d6880d352c70ba8d96b32f18b91fd5a6b
Jack Sun <m18205253820@163.com>
2017-01-10 16:16:03 +0800

    Update index.js

--- model/index.js
+++ model/index.js
@@ -2,7 +2,7 @@
  * Created by XYJ on 2016/2/26 0026.
  */
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://220.248.125.102:27017/live',function(err){console.log(err)});//���������ݿ�
+var db = mongoose.connect('mongodb://127.0.0.1:27017/live',function(err){console.log(err)});//���������ݿ�
 exports.users = mongoose.model('users', require('./user'));
 exports.options = mongoose.model('options', require('./options'));
 exports.handan = mongoose.model('handan', require('./handan'));
